e42b73779627decd54bb7ff5f07acca5e38c9688
Gleb Tv <glebtv@gmail.com>
2013-02-21 18:09:26 +0400

    don't fail when both formtastic and simple form are loaded

--- lib/uploader/hooks/formtastic.rb
+++ lib/uploader/hooks/formtastic.rb
@@ -1,12 +1,14 @@
+# encoding: utf-8
+
 require 'formtastic'
 
 class UploaderInput
-  include Formtastic::Inputs::Base
-  
+  include ::Formtastic::Inputs::Base
+
   def to_html
     input_wrapping do
       label_html <<
-      builder.uploader_field(method, input_html_options)
+          builder.uploader_field(method, input_html_options)
     end
   end
 end
