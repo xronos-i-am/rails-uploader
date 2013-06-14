aadd4c0a99d1181f6b0293c53cafb1aa6cbc5bf3
glebtv <glebtv@gmail.com>
2013-06-14 04:44:24 +0400

    Update application.js

--- app/assets/javascripts/uploader/application.js
+++ app/assets/javascripts/uploader/application.js
@@ -1,9 +1,6 @@
-//= require uploader/locales/en
 //= require uploader/tmpl
 //= require uploader/load-image
 //= require uploader/jquery.ui.widget
 //= require uploader/jquery.iframe-transport
 //= require uploader/jquery.fileupload
 //= require uploader/jquery.fileupload-ui
-
-
